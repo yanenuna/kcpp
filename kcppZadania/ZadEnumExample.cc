0783dc215ee5e5cd5c6dde4c8b2d9f7e2d7ad12e
yanenuna <yanenuna.mgmt@gmail.com>
2026-06-12 01:29:20 +0200

    Create ZadEnumExample.cc

---
+++ kcppZadania/ZadEnumExample.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Scoped enumeration for months
+ *
+ * This enum class represents months of the year.
+ */
+enum class Month {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+int main() {
+
+    /// Example of scoped enumeration
+    Month currentMonth = Month::June;
+
+    cout << "Value of June in Month: "
+         << static_cast<int>(currentMonth) << endl;
+
+    if (currentMonth == Month::June) {
+        cout << "Current month is June." << endl;
+    }
+
+    cout << "January: " << static_cast<int>(Month::January) << endl;
+    cout << "June: " << static_cast<int>(Month::June) << endl;
+    cout << "December: " << static_cast<int>(Month::December) << endl;
+
+    return 0;
+}
