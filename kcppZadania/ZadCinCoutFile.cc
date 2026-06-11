b143cb49436048ad2ffae7dc7f9caf0586517854
yanenuna <yanenuna.mgmt@gmail.com>
2026-06-11 23:41:03 +0200

    Create ZadCinCoutFile.cc

---
+++ kcppZadania/ZadCinCoutFile.cc
@@ -0,0 +1,18 @@
+```cpp
+#include <iostream>
+
+int main()
+{
+    // wypisanie informacji na ekran
+    std::cout << "program rozpoczyna dzialanie." << std::endl;
+    std::cout << "przykladowa wiadomosc wyswietlana przez cout." << std::endl;
+
+    // wypisanie komunikatu do strumienia bledow
+    std::cerr << "blad: przykladowy komunikat bledu." << std::endl;
+
+    // dalsza praca programu
+    std::cout << "program zakonczyl dzialanie." << std::endl;
+
+    return 0;
+}
+```
