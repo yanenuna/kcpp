a27a2ff1151fe915f6bc271fc1dc193846e43e7c
yanenuna <yanenuna.mgmt@gmail.com>
2026-06-12 01:37:40 +0200

    Create Tabela.cc

---
+++ kcppZadania/ZadClassKcpp/src/Tabela.cc
@@ -0,0 +1,14 @@
+#include "../include/Tabela.h"
+
+#include <iostream>
+
+using namespace std;
+
+void Tabela::wykonaj() {
+
+  cout << "Produkt\tCena" << endl;
+  cout << "Mleko\t4.50 zl" << endl;
+  cout << "Chleb\t3.00 zl" << endl;
+  cout << "Maslo\t8.00 zl" << endl;
+  cout << "Jajka\t12.00 zl" << endl;
+}
